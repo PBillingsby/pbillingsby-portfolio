a929f452a189fee1c87b93beb29b617d63ae896f
Phil Billingsby <philbillingsby@gmail.com>
2020-05-19 15:58:10 -0400

    Add footer links. Make nav links ajax

--- index.js
+++ index.js
@@ -2,24 +2,22 @@ document.addEventListener("DOMContentLoaded", function(){
   document.getElementById('projects').style.display = "none";
   getLinks();
 })
-const mainInfo = document.getElementById('main-information');
-
 function getLinks() {
-  let listItems = document.getElementById('nav');
-
-  listItems.addEventListener('click', function(e) {
-    e.preventDefault();
+  document.getElementById('nav').addEventListener('click', function(e) {
     let linkTo = e.target.innerText.toLowerCase();
     if (linkTo === "projects") {
-      debugger
+      document.getElementById('about').style.display = "none";
       document.getElementById('projects').style.display = "";
     }
     else {
       document.getElementById('about').style.display = "";
+      document.getElementById('projects').style.display = "none";
+
     }
-    // document.getElementById(linkTo).style.display = "none";
-    debugger
-    mainInfo.innerHTML = document.getElementById('projects').innerHTML;
+    // // document.getElementById(linkTo).style.display = "none";
+    // const mainInfo = document.getElementById('main-information');
+
+    // mainInfo.innerHTML = document.getElementById('projects').innerHTML;
   })
 }
 document.getElementsByTagName('img');
