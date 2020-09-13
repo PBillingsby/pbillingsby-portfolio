102bc14ff938319595ebb89569c3d9beac6a6229
PBillingsby <philbillingsby@gmail.com>
2020-09-13 12:38:25 -0700

    Add about content. Add fonts

--- portfolio/src/About.js
+++ portfolio/src/About.js
@@ -3,7 +3,29 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <h1>About</h1>
+      <h1 className="text-center">G'day</h1>
+      <div id="about-intro">
+        <p>
+          My name is Phil. I am a Full-Stack Developer located in Clark County,
+          Nevada. Originally from Australia I relocated to USA in 2017 and found
+          my passion for Software Development.
+        </p>
+        <p>
+          After dabbling with Python and teaching myself core computer
+          fundamental concepts I enrolled in Flatiron School's Software
+          Engineering course. I then discovered that I thoroughly enjoy
+          automating anything I possibly can and get immense joy from problem
+          solving and creating web applications from scratch.
+        </p>
+        <p>
+          I am a huge fan of the open source community and developers striving
+          to expand the industry to make innovative, useful products to better
+          the world of tech.
+        </p>
+      </div>
+      <div id="about-skills">
+        <p>SKILLS</p>
+      </div>
     </div>
   );
 };
